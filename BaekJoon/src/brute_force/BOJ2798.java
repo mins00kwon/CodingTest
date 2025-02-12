57144ea3832c4be8a85d4ebcc0e5c1348c6f8de6
mins00kwon <mskwon0918@gmail.com>
2025-02-12 20:26:40 +0900

    dfg

---
+++ BaekJoon/src/brute_force/BOJ2798.java
@@ -0,0 +1,49 @@
+package brute_force;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2798 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int n=Integer.parseInt(st.nextToken());
+        int m=Integer.parseInt(st.nextToken());
+        int[] arr=new int[n];
+        st=new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        int answer=0,firstCard,secondCard,thirdCard;
+        int tempAnswer;
+        for(int i=0;i<n;i++){
+            firstCard=arr[i];
+            if (firstCard>m){
+                continue;
+            }
+            for(int j=i+1;j<n;j++){
+                secondCard=arr[j];
+                if(firstCard+secondCard>m){
+                    continue;
+                }
+                for(int k=j+1;k<n;k++){
+                    thirdCard=arr[k];
+                    tempAnswer=firstCard+secondCard+thirdCard;
+                    if(tempAnswer>m){
+                        continue;
+                    }
+                    if((m-answer)>(m-tempAnswer)){
+                        if(tempAnswer==m){
+                            System.out.println(tempAnswer);
+                            return;
+                        }
+                        answer=tempAnswer;
+                    }
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
--- FirstStudy/src/main/java/practice_problems/BOJ7568.java
+++
@@ -1,4 +0,0 @@
-package practice_problems;
-
-public class BOJ7568 {
-}
